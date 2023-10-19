2147376d7e19b9b8c1d77a07f9ec72789a4cc87f
Aarav <2023a6r060@mietjammu.in>
2023-10-19 16:11:27 +0530

    first three hello world programs

---
+++ Arithmetic_operators/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,sum,sub,mult,div;
+   printf("enter the first number:-");
+   scanf("%d",&a);
+   printf("enter the second number:-");
+   scanf("%d",&b);
+   sum=a+b;
+   sub=a-b;
+   mult=a*b;
+   div=a/b;
+   printf("\n sum is %d",sum);
+   printf("\n sub is %d",sub);
+   printf("\n mult is %d",mult);
+   printf("\n div is %d",div);
+return 0;
+}
